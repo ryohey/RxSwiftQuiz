b4a986e916c40286b1cc99777c5053d5d6d172d1
mironal <miro.gunp@gmail.com>
2021-07-07 12:16:00 +0900

    Initial Commit

---
+++ RxSwiftQuiz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RxSwiftQuiz
+//
+//  Created by 山口勝也 on 2021/07/07.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RxSwiftQuiz/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RxSwiftQuiz
+//
+//  Created by 山口勝也 on 2021/07/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
